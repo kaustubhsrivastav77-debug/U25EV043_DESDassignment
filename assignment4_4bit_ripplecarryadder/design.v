5ca0f3e928b6984442746c8ed039c83a2099e986
Kaustubh <kaustubhsrivastav77@gmail.com>
2026-06-04 23:23:33 +0530

    Update design.v

--- assignment4_4bit_ripplecarryadder/design.v
+++ assignment4_4bit_ripplecarryadder/design.v
@@ -1,3 +1,16 @@
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+
+assign sum = a ^ b ^ cin;
+assign cout = (a & b) | (b & cin) | (a & cin);
+
+endmodule
+
 module ripple_carry_adder_4bit(
     input [3:0] A,
     input [3:0] B,
